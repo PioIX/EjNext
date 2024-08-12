f11c24db1a0408ded957cbe6b3b4297e84f4a134
EzeGreco <egreco@pioix.edu.ar>
2024-08-12 10:01:24 -0300

    Clase

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,15 +1,40 @@
 "use client"
 import ButtonClass from "@/components/class";
+import Button from "@/components/button";
+import Checkbox from "@/components/checkbox";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  let [contador, setCuenta] = useState=
+  let [contador, setCuenta] = useState(0)
+  let [nombre, setNombre] = useState("Anon")
+
   function funcionSumar(){
     setCuenta(contador++)
+  }
 
+  function funcionRestar(){
+    setCuenta(contador--)
+  }
+
+  function funcionNombre(){
+    let nNombre=document.getElementById("ingresoNombre").value
+    setNombre(nNombre)
   }
   function funcionB(){
     console.log("hola chungo")
   }
+
+  useEffect(()=>{
+    setCuenta(10)
+  }, []
+)
+
+useEffect(()=>{
+  setCuenta(contador+=5)
+}, [nombre]
+)
+
   /*
 
   return (
@@ -27,6 +52,11 @@ export default function Home() {
       <ButtonClass textTitle={"FaconApps"} textSubtitle={"Marchesi es el unico que trabaja"} nameTeacher={"Nicola Facón"} expireDate={"2/10"} taskName={"Tp final"} onClick={funcionB}></ButtonClass>
       <ButtonClass textTitle={"FaconApps"} textSubtitle={"Marchesi es el unico que trabaja"} nameTeacher={"Nicola Facón"} expireDate={"2/10"} taskName={"Tp final"} onClick={funcionB}></ButtonClass>
       <ButtonClass textTitle={"FaconApps"} textSubtitle={"Marchesi es el unico que trabaja"} nameTeacher={"Nicola Facón"} expireDate={"2/10"} taskName={"Tp final"} onClick={funcionB}></ButtonClass>
+      <Checkbox value={"checked"}></Checkbox>
+      <h2> contador: { contador }</h2>
+      <h2> nombre: { nombre }</h2>
+      <input type="text" id="ingresoNombre" placeholder="pepeado"></input>
+      <Button onClick={funcionNombre}> modificar nombre</Button>
     </main>
 
 
---
+++ src/components/checkbox.js
@@ -0,0 +1,10 @@
+"use client"
+
+//props
+export default function Checkbox(props) {
+    
+    return (
+        <input type="checkbox" {props.value}></input>
+
+    );
+  };
